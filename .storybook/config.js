0587939a5b809991c89c7f7f8ae7d2141b7bad52
Joshua Comeau <joshwcomeau@gmail.com>
2017-02-07 08:40:19 -0500

    Updates

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@kadira/storybook';
-
-const req = require.context('../stories', true, /.stories.js$/);
-
-function loadStories() {
-  req.keys().forEach((filename) => req(filename));
-}
-
-configure(loadStories, module);
--- src/helpers/misc.helpers.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-restricted-properties
-export const generateId = () => Math.round(Math.random() * Math.pow(10, 16));
