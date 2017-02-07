6aa42330bea921f5898fa2b58eb73034b0f9a855
Joshua Comeau <joshwcomeau@gmail.com>
2017-02-07 08:28:45 -0500

    Demo tweak

--- demo/src/components/Header/Header.js
+++ demo/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => (
   <header className="header">
     <h1>React Trend</h1>
-    <p>Elegant, simple <em>trend</em> graphs for React.js.</p>
+    <p>Simple, elegant trend graphs for React.js.</p>
     <p>A project by <a href="https://www.unsplash.com">Unsplash</a>.</p>
   </header>
 );
