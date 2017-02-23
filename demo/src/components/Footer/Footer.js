06f46318d71fe33184b85d8533369036949b4a57
Joshua Comeau <joshwcomeau@gmail.com>
2017-02-23 11:05:11 -0500

    Remove unnecessary copyright notice from demo page

--- demo/src/components/Footer/Footer.js
+++ demo/src/components/Footer/Footer.js
@@ -4,8 +4,6 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    Copyright 2017-present <a href="https://www.unsplash.com">Unsplash</a>. All Rights Reserved.
-    <br />
     Released under the <a href="https://github.com/unsplash/react-trend/blob/master/LICENSE.md">MIT</a> license. <a href="https://github.com/unsplash/react-trend">View source</a>.
   </footer>
 );
